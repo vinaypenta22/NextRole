fc488c92d516fedff07b6ba1b0dc49adaffad2df
vinay <vinay.p@pranathiss.com>
2026-07-16 12:49:53 +0530

    updated register

--- Backend/mainapp/userside/admin.py
+++ Backend/mainapp/userside/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import userModel
+from .models import userModel , Resume , Job , AppliedJob
 # Register your models here.
 admin.site.register(userModel)
+admin.site.register(Resume)
+admin.site.register(Job)
+admin.site.register(AppliedJob)
